0aa0529e593deec8480bb10fee32af05aebbc6fd
Lee <854001722@qq.com>
2017-08-29 11:53:04 +0800

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,37 @@
+# ToJson
+ArraysToJson，CollectionsToJson，MapToJson，XMLToJson
+//构建一个学生对象
+public class Student {
+    private String userName;
+    private String sex;
+    private int age;
+   
+    public Student() {
+    }
+   
+    public Student(String userName, String sex, int age) {
+        this.userName = userName;
+        this.sex = sex;
+        this.age = age;
+    }
+ 
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getSex() {
+        return sex;
+    }
+   
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
