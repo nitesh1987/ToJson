047b20ee9fc7baabe570c3a491878718f8257ea7
Lee <854001722@qq.com>
2017-08-29 11:54:16 +0800

    Create Class.java

---
+++ Class.java
@@ -0,0 +1,36 @@
+# ToJson
+ArraysToJson，CollectionsToJson，MapToJson，XMLToJson
+//测试实体类
+public class Class {
+    private String name;
+    private Date date;
+    private List<Student> students;
+   
+    public Class() {
+    }
+   
+    public Class(String name, Date date, List<Student> students) {
+        this.name = name;
+        this.date = date;
+        this.students = students;
+    }
+   
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public List<Student> getStudents() {
+        return students;
+    }
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+}
